7a213ae4ba6b62858f690afbf51023dbca140002
Val R <valr@Vals-MacBook-Pro-2.local>
2019-06-20 10:34:48 -0700

    fixed all bugs I think

--- createFields.py
+++ createFields.py
@@ -13,7 +13,7 @@ def sendReq(url, querystring):
         'Upgrade-Insecure-Requests': "1",
         'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36",
         'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-        'Referer': "http://bugzilla.int.peritus.ai:9080/bugzilla/editfields.cgi?action=edit&name=cf_test",
+        'Referer': "http://bugzilla.int.peritus.ai:9080/bugzilla/editfields.cgi",
         'Accept-Encoding': "gzip, deflate",
         'Accept-Language': "en-US,en;q=0.9",
         'Cookie': "TUI=expert_fields=1&attachment_text_field=0; DEFAULTFORMAT=specific; LASTORDER=bug_status%2Cpriority%2Cassigned_to%2Cbug_id; BUGLIST=1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24-25-26-27-28-29-30-31-32-33-34-35-36-37-38-39-40-41-42-43-44-45-46-47-48-49-50-51-52-53-54-55-56-57-58-59-60-61-62-63-64-65-66-67-68-69-70-71-72-73-74-75-76-77-78-79-80-81-82-83-84-85-86-87-88-89-90-91-92-93-94-95-96-97-98-99-100-101-102-103-104-105-106-107-108-109-110-111-112-113-114-115-116-117-118-119-120-121-122-123-124-125-126-127-128-129-130-131-132-133-134-135-136-137-138-139-140-141-142-143-144-145-146-147-148-149-150-151-152-153-154-155-156-157-158-159-160-161-162-163-164-165-166-167-168-169-170-171-172-173-174-175-176-177-178-179-180-181-182-183-184-185-186-187-188-189-190-191-192-193-194-195-196-197-198-199-200-201-202-203-204-205-206-207-208-209-210-211-212-213-214-215-216-217-218-219-220-221-222-223-224-225-226-227-228-229-230-231-232-233-234-235-236-237-238-239-240-241-242-243-244-245-246-247-248-249-250-251-252-253-254-255-256-257-258-259-260-261-262-263-264-265-266-267-268-269-270-271-272-273-274-275-276-277-278-279-280-281-282-283-284-285-286-287-288-289-290-291-292-293-294-295-296-297-298-299-300-301-302-303-304-305-306-307-308-309-310-311-312-313-314-315-316-317-318-319-320-321-322-323-324-325-326-327-328-329-330-331-332-333-334-335-336-337-338-339-340-341-342-343-344-345-346-347-348-349-350-351-352-353-354-355-356-357-358-359-360-361-362-363-364-365-366-367-368-369-370-371-372-373-374-375-376-377-378-379-380-381-382-383-384-385-386-387-388-389-390-391-392-393-394-395-396-397-398-399-400-401-402-403-404-405-406-407-408-409-410-411-412-413-414-415-416-417-418-419-420-421-422-423-424-425-426-427-428-429-430-431-432-433-434-435-436-437-438-439-440-441-442-443-444-445-446-447-448-449-450-451-452-453-454-455-456-457-458-459-460-461-462-463-464-465-466-467-468-469-470-471-472-473-474-475-476-477-478-479-480-481-482-483-484-485-486-487-488-489-490-491-492-493-494-495-496-497-498-499-500; Bugzilla_login=1; Bugzilla_logincookie=aknTv2OqZT; Bugzilla_login_request_cookie=0QXffo7DJ0",
@@ -24,7 +24,7 @@ def sendReq(url, querystring):
         "GET", url, headers=headers, params=querystring)
 
 
-def sendOptionReq(fieldname, optionvalue, token):
+def sendOptionCreateReq(fieldname, optionvalue, token):
 
     data = {
         'value': optionvalue,
@@ -33,6 +33,7 @@ def sendOptionReq(fieldname, optionvalue, token):
         'field': fieldname,
         'token': token
     }
+    print(data)
     url = "http://bugzilla.int.peritus.ai:9080/bugzilla/editvalues.cgi"
 
     headers = {
@@ -41,17 +42,15 @@ def sendOptionReq(fieldname, optionvalue, token):
         'Upgrade-Insecure-Requests': "1",
         'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36",
         'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-        'Referer': "http://bugzilla.int.peritus.ai:9080/bugzilla/editfields.cgi?action=edit&name=cf_test",
+        'Referer': "http://bugzilla.int.peritus.ai:9080/bugzilla/editvalues.cgi",
         'Accept-Encoding': "gzip, deflate",
         'Accept-Language': "en-US,en;q=0.9",
         'Cookie': "TUI=expert_fields=1&attachment_text_field=0; DEFAULTFORMAT=specific; LASTORDER=bug_status%2Cpriority%2Cassigned_to%2Cbug_id; BUGLIST=1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24-25-26-27-28-29-30-31-32-33-34-35-36-37-38-39-40-41-42-43-44-45-46-47-48-49-50-51-52-53-54-55-56-57-58-59-60-61-62-63-64-65-66-67-68-69-70-71-72-73-74-75-76-77-78-79-80-81-82-83-84-85-86-87-88-89-90-91-92-93-94-95-96-97-98-99-100-101-102-103-104-105-106-107-108-109-110-111-112-113-114-115-116-117-118-119-120-121-122-123-124-125-126-127-128-129-130-131-132-133-134-135-136-137-138-139-140-141-142-143-144-145-146-147-148-149-150-151-152-153-154-155-156-157-158-159-160-161-162-163-164-165-166-167-168-169-170-171-172-173-174-175-176-177-178-179-180-181-182-183-184-185-186-187-188-189-190-191-192-193-194-195-196-197-198-199-200-201-202-203-204-205-206-207-208-209-210-211-212-213-214-215-216-217-218-219-220-221-222-223-224-225-226-227-228-229-230-231-232-233-234-235-236-237-238-239-240-241-242-243-244-245-246-247-248-249-250-251-252-253-254-255-256-257-258-259-260-261-262-263-264-265-266-267-268-269-270-271-272-273-274-275-276-277-278-279-280-281-282-283-284-285-286-287-288-289-290-291-292-293-294-295-296-297-298-299-300-301-302-303-304-305-306-307-308-309-310-311-312-313-314-315-316-317-318-319-320-321-322-323-324-325-326-327-328-329-330-331-332-333-334-335-336-337-338-339-340-341-342-343-344-345-346-347-348-349-350-351-352-353-354-355-356-357-358-359-360-361-362-363-364-365-366-367-368-369-370-371-372-373-374-375-376-377-378-379-380-381-382-383-384-385-386-387-388-389-390-391-392-393-394-395-396-397-398-399-400-401-402-403-404-405-406-407-408-409-410-411-412-413-414-415-416-417-418-419-420-421-422-423-424-425-426-427-428-429-430-431-432-433-434-435-436-437-438-439-440-441-442-443-444-445-446-447-448-449-450-451-452-453-454-455-456-457-458-459-460-461-462-463-464-465-466-467-468-469-470-471-472-473-474-475-476-477-478-479-480-481-482-483-484-485-486-487-488-489-490-491-492-493-494-495-496-497-498-499-500; Bugzilla_login=1; Bugzilla_logincookie=aknTv2OqZT; Bugzilla_login_request_cookie=0QXffo7DJ0",
         'Cache-Control': "no-cache",
-        'Postman-Token': "dcc5b8e5-6e72-4855-92e9-63ea4260e4cb,900efec2-e0ec-460a-a081-8c70862d1eb8",
-        'content-type': "application/form-data;",
-        'cache-control': "no-cache"
+        'Content-type': "application/x-www-form-urlencoded",
         }
 
-    response = requests.request("POST", url, data=payload, headers=headers)
+    response = requests.request("POST", url, data=data, headers=headers)
     return response
 
 
@@ -60,6 +59,8 @@ def getToken(text, xp):
     tree = html.fromstring(text)
     token = tree.xpath(xp)
     # token = tree.xpath('//*[@id="type"]')
+
+    print(token)
 
     return token[0]
 
@@ -77,12 +78,17 @@ def getFieldToken():
 
 def getOptionToken(fieldname):
 
+    print('creating option token')
+
     url = "http://bugzilla.int.peritus.ai:9080/bugzilla/editvalues.cgi"
     querystring = {"action": "add", "field": fieldname}
 
     response = sendReq(url, querystring)
 
-    token = getToken(response.text, '//*[@id="add_field"]/p/input[2]/@value')
+    # print(response.text)
+
+    token = getToken(
+        response.text, '//*[@id="bugzilla-body"]/form/input[4]/@value')
     return token
 
 
@@ -92,11 +98,16 @@ def loadFields():
     with open('fields.json') as json_file:
         data = json.load(json_file)
         fields = data['fields']
+
+    for field in fields:
+        field['name'] = 'cf_' + field['name']
+
     return fields
 
 
 def createSimpleField(fieldBean):
 
+    print(' - Creating simple field : %s' % fieldBean['name'])
     token = getFieldToken()
     field = Field(fieldBean['name'], fieldBean['type'], token)
 
@@ -106,37 +117,47 @@ def createSimpleField(fieldBean):
 
     response = sendReq(url, querystring)
 
-    return response.status_code
+    # print(response.text)
+
+    return response
 
 
 def createDropdownOption(fieldBean, option):
 
-    token = getOptionToken(option)
-    response = sendOptionReq(fieldBean['name'], option, token)
+    print(' - Creating option : "%s" for %s' % (option, fieldBean['name']))
+    token = getOptionToken(fieldBean['name'])
+    response = sendOptionCreateReq(fieldBean['name'], option, token)
 
-    return response.text
+    # print(response.text)
+
+    return response
 
 
 def createDropdownField(fieldBean):
 
+    print(' - Creating dropdown field : %s' % fieldBean['name'])
     response = createSimpleField(fieldBean)
+
+    # print(response.text)
 
     for option in fieldBean['options']:
         createDropdownOption(fieldBean, option)
 
-    return response.status_code
+    return response
 
 
 def createFields(fieldBeans):
 
     n = 0
     for fieldBean in fieldBeans:
+
+        print('Loading field : %s; Type %s' % (fieldBean['name'], fieldBean['type']))
         if fieldBean['type'] == '2':
             createDropdownField(fieldBean)
         else:
             createSimpleField(fieldBean)
-        print('%s / %s' % (n, len(fieldBeans)))
         n += 1
+        print('%s / %s' % (n, len(fieldBeans)))
     return 'done'
 
 
@@ -145,16 +166,16 @@ def main():
     fields = loadFields()
     print('Loaded %s fields' % len(fields))
 
-    print(fields[:4])
+    # print(fields)
 
-    createFields(fields[:4])
+    createFields(fields)
     # status = submitField(field)
     # print('Field submitted : %s' % status)
 
 
 class Field:
     def __init__(self, name, type, token, enter_bug=1, desc=None, obsolete=0, sortkey=None, long_desc=None, visibility_field_id=None, action='new'):
-        self.name = 'cf_' + name
+        self.name = name
         self.token = token
         self.type = type
 
@@ -166,7 +187,7 @@ class Field:
         if desc:
             self.desc = desc
         else:
-            self.desc = name
+            self.desc = name[3:]
 
         if obsolete:
             self.obsolete = obsolete
